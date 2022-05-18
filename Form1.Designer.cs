7ce0c8cb4357994602a5e58b9e63371f2fab2ba3
Devita-work <devita.work@yandex.ru>
2022-05-18 13:42:25 +0400

    First ever

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace дом_стр_322
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.description = new System.Windows.Forms.TextBox();
+            this.exist = new System.Windows.Forms.ComboBox();
+            this.goHere = new System.Windows.Forms.Button();
+            this.goThroughtTheDoor = new System.Windows.Forms.Button();
+            this.check = new System.Windows.Forms.Button();
+            this.Hide = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(37, 12);
+            this.description.Multiline = true;
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(291, 185);
+            this.description.TabIndex = 0;
+            // 
+            // exist
+            // 
+            this.exist.FormattingEnabled = true;
+            this.exist.Location = new System.Drawing.Point(207, 212);
+            this.exist.Name = "exist";
+            this.exist.Size = new System.Drawing.Size(121, 24);
+            this.exist.TabIndex = 1;
+            this.exist.SelectedIndexChanged += new System.EventHandler(this.exist_SelectedIndexChanged);
+            // 
+            // goHere
+            // 
+            this.goHere.Location = new System.Drawing.Point(37, 212);
+            this.goHere.Name = "goHere";
+            this.goHere.Size = new System.Drawing.Size(75, 23);
+            this.goHere.TabIndex = 2;
+            this.goHere.Text = "Go here";
+            this.goHere.UseVisualStyleBackColor = true;
+            this.goHere.Click += new System.EventHandler(this.goHere_Click);
+            // 
+            // goThroughtTheDoor
+            // 
+            this.goThroughtTheDoor.Location = new System.Drawing.Point(37, 253);
+            this.goThroughtTheDoor.Name = "goThroughtTheDoor";
+            this.goThroughtTheDoor.Size = new System.Drawing.Size(291, 24);
+            this.goThroughtTheDoor.TabIndex = 3;
+            this.goThroughtTheDoor.Text = "Go throught the door";
+            this.goThroughtTheDoor.UseVisualStyleBackColor = true;
+            this.goThroughtTheDoor.Visible = false;
+            this.goThroughtTheDoor.Click += new System.EventHandler(this.goThroughtTheDoor_Click);
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(37, 298);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(291, 24);
+            this.check.TabIndex = 4;
+            this.check.Text = "check";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // Hide
+            // 
+            this.Hide.Location = new System.Drawing.Point(37, 342);
+            this.Hide.Name = "Hide";
+            this.Hide.Size = new System.Drawing.Size(291, 28);
+            this.Hide.TabIndex = 5;
+            this.Hide.Text = "Hide";
+            this.Hide.UseVisualStyleBackColor = true;
+            this.Hide.Click += new System.EventHandler(this.Hide_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Hide);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.goThroughtTheDoor);
+            this.Controls.Add(this.goHere);
+            this.Controls.Add(this.exist);
+            this.Controls.Add(this.description);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.ComboBox exist;
+        private System.Windows.Forms.Button goHere;
+        private System.Windows.Forms.Button goThroughtTheDoor;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Button Hide;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace дом_стр_322
+{
+    public partial class Form1 : Form
+    {
+        abstract class Location
+        {
+            public Location(string name)
+            {
+                this.name = name;
+            }
+
+            public virtual bool IsLocked
+            {
+                get { return false; }
+            }
+
+            public Location[] Exits;
+            private string name;
+            public string Name
+            {
+                get { return name; }
+            }
+            public virtual string Description
+            {
+                get
+                {
+                    string description = "Вы находитесь в " + name + ". Вы видите двери, ведущие в: ";
+                    for (int i = 0; i < Exits.Length; i++)
+                    {
+                        description += " " + Exits[i].Name;
+                        if (i != Exits.Length - 1)
+                            description += ",";
+                    }
+                    description += ".";
+                    return description;
+                }
+                set { string description = value; }
+            }
+        }
+
+        interface IHasExteriorDoor
+        {
+            string DoorDescription { get; }
+            Location DoorLocation { get; set; }
+        }
+
+        class Attic : Location
+        {
+            private string decoration;
+            public Attic(string name, string decoration, bool islocked) : base(name)
+            {
+                this.decoration = decoration;
+            }
+
+            public override string Description
+            {
+                get
+                {
+                    return base.Description + " Вы видите " + decoration + ".";
+                }
+            }
+        }
+
+        class Room : Location
+        {
+            private string decoration;
+            public Room(string name, string decoration) : base(name)
+            {
+                this.decoration = decoration;
+            }
+
+            public override string Description
+            {
+                get
+                {
+                    return base.Description + " Вы видите " + decoration + ".";
+                }
+            }
+        }
+
+        interface IHidingPlace
+        {
+            string HidingPlaceName { get; }
+        }
+
+        class RoomWithHidingPlace : Room, IHidingPlace
+        {
+            public RoomWithHidingPlace(string name, string decoration, string hidingPlaceName) : base(name, decoration)
+            {
+                this.hidingPlaceName = hidingPlaceName;
+            }
+            private string hidingPlaceName;
+            public string HidingPlaceName
+            {
+                get { return hidingPlaceName; }
+            }
+
+            public override string Description
+            {
+                get { return base.Description + " Спрятаться можно " + hidingPlaceName + "."; }
+            }
+        }
+
+        class RoomWithDoor : RoomWithHidingPlace, IHasExteriorDoor
+        {
+            public RoomWithDoor(string name, string decoration, string hidingPlaceName, string doorDescription) : base(name, decoration, hidingPlaceName)
+            {
+                this.doorDescription = doorDescription;
+            }
+
+            private string doorDescription;
+            public string DoorDescription
+            {
+                get { return doorDescription; }
+            }
+            private Location doorLocation;
+            public Location DoorLocation
+            {
+                get { return doorLocation; }
+                set { doorLocation = value; }
+            }
+
+        }     
+
+        class WindowAttic : Location
+        {
+            private string name;
+            private string description;
+            public WindowAttic (string name, string description) : base (name)
+            {
+                this.name = name;
+                this.description = description;
+            }
+
+            public override string Description
+            {
+                get
+                {
+                    return "Убегая от монстра вы вылетели через окно на чердаке, если бы не дерево, растущее на заднем дворе вы бы точно разбились. " + "Единственный путь спуститься по дереву на задний двор";
+                }
+
+
+            }
+        }
+
+        class Outside : Location
+        {
+            private bool hot;
+            public bool Hot { get { return hot; } }
+
+            public Outside(string name, bool hot) : base(name)
+            {
+                this.hot = hot;
+            }
+            public override string Description
+            {
+                get
+                {
+                    string NewDescription = base.Description;
+                    if (hot)
+                        NewDescription += " Очень жарко.";
+                    return NewDescription;
+                }
+                set { base.Description = value; }
+            }
+        }
+
+        class OutsideWithHidingPlace : Outside, IHidingPlace
+        {
+            public OutsideWithHidingPlace(string name, bool hot, string hidingPlaceName):base(name, hot)
+            {
+                this.hidingPlaceName = hidingPlaceName;
+            }
+            private string hidingPlaceName;
+            public string HidingPlaceName
+            {
+                get { return hidingPlaceName; }
+            }
+            public override string Description
+            {
+                get { return base.Description + " Можно спрятаться " + hidingPlaceName + "."; }
+            }
+        }
+
+        class Opponent
+        {
+            private Random random;
+            private Location myLocation;
+            public Opponent(Location startingLocation)
+            {
+                myLocation = startingLocation;
+                random = new Random();
+            }
+            public void Move()
+            {
+                if (myLocation is IHasExteriorDoor)
+                {
+                    IHasExteriorDoor LocationWithDoor = myLocation as IHasExteriorDoor;
+                    if (random.Next(2) == 1)
+                        myLocation = LocationWithDoor.DoorLocation;
+                }
+                bool hidden = false;
+                while (!hidden)
+                {
+                    int rand = random.Next(myLocation.Exits.Length);
+                    myLocation = myLocation.Exits[rand];
+                    if (myLocation is IHidingPlace)
+                        hidden = true;
+                }
+            }
+            public bool Check(Location locationToCheck)
+            {
+                if (locationToCheck != myLocation)
+                    return false;
+                else
+                    return true;
+            }
+        }
+        class OutsideWithDoor : Outside, IHasExteriorDoor
+        {
+            public OutsideWithDoor(string name, bool hot, string doorDescription) : base(name, hot)
+            {
+                this.doorDescription = doorDescription;
+            }
+            private string doorDescription;
+            public string DoorDescription
+            {
+                get { return doorDescription; }
+            }
+            private Location doorLocation;
+            public Location DoorLocation
+            {
+                get { return doorLocation; }
+                set { doorLocation = value; }
+            }
+            public override string Description
+            {
+                get { return base.Description + " Вы видите " + doorDescription + "."; }
+                set { base.Description = value; }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            CreateObjects();
+            opponent = new Opponent(frontYard);
+            ResetGame(false);
+        }
+        int Moves;
+        Location currentLocation;
+        RoomWithDoor livingRoom;
+        RoomWithHidingPlace diningRoom;
+        RoomWithDoor kitchen;
+        Room stairs;
+        RoomWithHidingPlace hallway;
+        RoomWithHidingPlace bathroom;
+        RoomWithHidingPlace masterBedroom;
+        RoomWithHidingPlace secindBedroom;
+
+        WindowAttic window;
+        OutsideWithDoor frontYard;
+        OutsideWithDoor backYard;
+        OutsideWithHidingPlace garden;
+        OutsideWithHidingPlace driveway;
+
+        Attic attic;
+
+        Opponent opponent;        
+
+        private void MoveToANewLocation(Location newlocation)
+        {
+            Moves++;
+            if ((currentLocation == attic) & (newlocation == livingRoom)) {
+                MessageBox.Show("Кто-то закрыл за вами дверь в гостинную! Ищи другой способ выбраться!");
+            }
+            else {
+                currentLocation = newlocation;
+                RedrawForm();
+            }
+        }
+
+        private void RedrawForm()
+        {
+            exist.Items.Clear();
+            for (int i = 0; i < currentLocation.Exits.Length; i++)
+                exist.Items.Add(currentLocation.Exits[i].Name);
+            exist.SelectedIndex = 0;
+            description.Text = currentLocation.Description + "\r\n(перемещение #" + Moves + ")";
+            if (currentLocation is IHidingPlace)
+            {
+                IHidingPlace hidingPlace = currentLocation as IHidingPlace;
+                check.Text = "Check " + hidingPlace.HidingPlaceName;
+                check.Visible = true;
+            }
+            else
+            {
+                check.Visible = false;
+                check.Text = "";
+
+            }
+            if (currentLocation is IHasExteriorDoor)
+                goThroughtTheDoor.Visible = true;
+            else
+                goThroughtTheDoor.Visible = false;
+            if (currentLocation == attic)
+            {
+                
+                timer1.Stop();
+                timer1.Interval = 5000;
+                timer1.Enabled = true;
+                MessageBox.Show("На чердаке жуткий монстр! Еще немного и он вас съест!");
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void CreateObjects()
+        {
+            livingRoom = new RoomWithDoor("Гостиная", "старинный ковер","в гардеробе", "дубовая дверь с латунной ручкой");
+            diningRoom = new RoomWithHidingPlace("Столовая", "хрустальная люстра", "в высоком шкафу");
+            kitchen = new RoomWithDoor("Кухня", "плита из нержавеющей стали", "в сундуке", "сетчатая дверь");
+            stairs = new Room("Лестница", "деревянные перила");
+            hallway = new RoomWithHidingPlace("Верхний коридор", "картина с собакой", "в гардеробе");
+            bathroom = new RoomWithHidingPlace("Ванная", "раковина и туалет", "в душе");
+            masterBedroom = new RoomWithHidingPlace("Главная спальня", "большая кровать", "под кроватью");
+            secindBedroom = new RoomWithHidingPlace("Вторая спальня", "маленькая кровать", "под кроватью");
+            frontYard = new OutsideWithDoor("лужайка", false, " тяжелая дубовая дверь");
+            backYard = new OutsideWithDoor("Задний двор", true, "сетчатая дверь");
+            window = new WindowAttic("Окно", "Убегая от монстра вы вылетели через окно на чердаке, если бы не дерево, растущее на заднем дворе вы бы точно разбились.");
+            garden = new OutsideWithHidingPlace("Сад", false, "в сарае");
+            driveway = new OutsideWithHidingPlace("Подъезд", true, "в гараже");
+
+            attic = new Attic("Чердак", "Слабо освещен приоткрытым окном", false);
+
+            window.Exits = new Location[] { backYard };
+            attic.Exits = new Location[] { window, livingRoom};
+            diningRoom.Exits = new Location[] { livingRoom, kitchen };
+            livingRoom.Exits = new Location[] { diningRoom, stairs, attic };
+            kitchen.Exits = new Location[] { diningRoom};
+            stairs.Exits = new Location[] { livingRoom, hallway };
+            hallway.Exits = new Location[] { stairs, bathroom, masterBedroom, secindBedroom };
+            bathroom.Exits = new Location[] {hallway};
+            masterBedroom.Exits = new Location[] { hallway };
+            secindBedroom.Exits = new Location[] { hallway };
+            frontYard.Exits = new Location[] { backYard, garden, driveway};
+            backYard.Exits = new Location[] { frontYard, garden, driveway };
+            garden.Exits = new Location[] { backYard, frontYard };
+            driveway.Exits = new Location[] { backYard, frontYard };
+
+            livingRoom.DoorLocation = frontYard;
+            frontYard.DoorLocation = livingRoom;
+            kitchen.DoorLocation = backYard;
+            backYard.DoorLocation = kitchen;
+        }
+
+        private void ResetGame (bool displayMessage)
+        {
+            if (displayMessage)
+            {
+                MessageBox.Show("Меня нашли за " + Moves + " ходов!");
+                IHidingPlace foundLocation = currentLocation as IHidingPlace;
+                description.Text = "Соперник найден за " + Moves + " ходов! Он прятался в " + foundLocation.HidingPlaceName + '.';
+            }
+            Moves = 0;
+            Hide.Visible = true;
+            goHere.Visible = false;
+            check.Visible = false;
+            goThroughtTheDoor.Visible = false;
+            exist.Visible = false;
+        }
+
+        private void Reset(bool message)
+        {
+            if(message)
+            {
+                MessageBox.Show("Вы проиграли! Вас съел монстр");
+            }
+            Moves = 0;
+            Hide.Visible = true;
+            goHere.Visible = false;
+            check.Visible = false;
+            goThroughtTheDoor.Visible = false;
+            exist.Visible = false;
+        }
+
+        private void goHere_Click(object sender, EventArgs e)
+        {
+            MoveToANewLocation(currentLocation.Exits[exist.SelectedIndex]);
+        }
+
+        private void goThroughtTheDoor_Click(object sender, EventArgs e)
+        {
+            IHasExteriorDoor hasDoor = currentLocation as IHasExteriorDoor;
+            MoveToANewLocation(hasDoor.DoorLocation);
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            Moves++;
+            if (opponent.Check(currentLocation))
+                ResetGame(true);
+            else
+                RedrawForm();
+        }
+
+        private void Hide_Click(object sender, EventArgs e)
+        {
+            Hide.Visible = false;
+            for (int i = 0; i <= 10; i++)
+            {
+                opponent.Move();
+                description.Text = i + "... ";
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(200);
+            }
+            description.Text = "Я иду искать!";
+            Application.DoEvents();
+            System.Threading.Thread.Sleep(500);
+            goHere.Visible = true;
+            exist.Visible = true;
+            MoveToANewLocation(livingRoom);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Reset(true);
+            currentLocation = null;
+        }
+
+        private void exist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
